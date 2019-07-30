f02375656976e51226cdc8c05ec0171a19284e22
Carson Howard <cjhoward92@gmail.com>
2019-07-29 23:21:35 -0700

    Updated CF template

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# e - exit on error
+# r - restricted (as a security measure)
+# x - verbose and expand commands
+# u - error on unset variables
+# n - read the commands but don't execute them (remove when ready to run)
+set -enxur
+
+# This is the bash file used for deployment
